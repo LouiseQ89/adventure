bf6a948858f044717397c74b2bca66c3642c4bd6
Louise <louise04@btinternet.com>
2017-07-22 16:34:50 +0100

    fixes #1: check if user has entered a valid direction

--- PycharmProjects/adventure/adventure.py
+++ PycharmProjects/adventure/adventure.py
@@ -8,7 +8,6 @@ directions = {
 }
 
 position = (0, 0)
-
 while True:
     location = locations[position]
     print 'you are at the %s' % location
@@ -22,4 +21,9 @@ while True:
             valid_directions[k] = possible_position
 
     direction = raw_input('which direction do you want to go?\n')
-    position = valid_directions[direction]
+
+    new_position = valid_directions.get(direction)
+    if new_position:
+        position = new_position
+    else:
+        print "sorry, that isn't a valid direction"
